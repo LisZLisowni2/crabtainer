1303a9b97484a03de615a78f75227ca1121c4e18
lislisowni2 <rafalkozikowski735@gmail.com>
2026-08-09 09:49:07 +0200

    Hotfix of from.rs

--- src/engine/instructions/from.rs
+++ src/engine/instructions/from.rs
@@ -23,9 +23,9 @@ pub async fn from_image(base_image: &String, output_layout_name: &String) -> Res
 
     println!(" => [FROM] Extract rootfs from {} to {}'s rootfs", base_image, output_layout_name);
     let path = RustockerPaths::layout_store_dir().join(output_layout_name).join("rootfs");
-    
+
     std::fs::create_dir_all(&path).map_err(|e| format!("Error creating output directory: {}", e))?;
-    
+
     // TODO: Handle other types of zip (like .zip)
     let status = Command::new("tar")
         .args(["-xzf", &found_file.to_string_lossy(), "-C", &path.to_str().unwrap()])
